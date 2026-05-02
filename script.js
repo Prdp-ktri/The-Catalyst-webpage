d1d8af3a30ac5896a7307ca8eadd7f6cd2d529b0
Pradeep Khatri <pradeepcodes08@gmail.com>
2026-05-02 17:14:36 +0530

    commit navigation completed
    commit navigation completed

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-function handleClick(section) {
-  console.log("Clicked:", section);
+function handleClick(sectionId) {
+  const section = document.getElementById(sectionId);
 
-  switch (section) {
-    case "home":
-      alert("Go to Home");
-      break;
-    case "performance":
-      alert("Navigate to Performance");
-      break;
-    case "features":
-      alert("Navigate to Features");
-      break;
-    case "prices":
-      alert("Navigate to Prices");
-      break;
-    case "faq":
-      alert("Navigate to FAQ");
-      break;
-    case "login":
-      alert("Open Login Page");
-      break;
-    case "start":
-      alert("Start Here Clicked");
-      break;
+  if (section) {
+    section.scrollIntoView({
+      behavior: "smooth",
+    });
   }
 }
 
