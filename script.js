fd72d650c3edf7c54eff084eb4a4b236f943156e
Pradeep Khatri <pradeepcodes08@gmail.com>
2026-05-02 12:37:17 +0530

    commit engine section made
    commit engine section made

--- script.js
+++ script.js
@@ -25,3 +25,38 @@ function handleClick(section) {
       break;
   }
 }
+
+// Animate numbers using setTimeout
+
+function animateValue(id, start, end, duration) {
+  let range = end - start;
+  let current = start;
+  let increment = range / (duration / 20);
+
+  function update() {
+    current += increment;
+
+    if (
+      (increment > 0 && current >= end) ||
+      (increment < 0 && current <= end)
+    ) {
+      document.getElementById(id).innerText = end;
+      return;
+    }
+
+    document.getElementById(id).innerText = Math.floor(current);
+    setTimeout(update, 20);
+  }
+
+  update();
+}
+
+// Run animations
+setTimeout(() => animateValue("stat1", 0, 42, 800), 200);
+setTimeout(() => animateValue("stat2", 0, 99, 1200), 400); // will format later
+setTimeout(() => animateValue("stat3", 0, 10, 800), 600);
+
+// Fix decimal for 99.99%
+setTimeout(() => {
+  document.getElementById("stat2").innerText = "99.99";
+}, 1300);
